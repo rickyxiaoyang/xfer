2bc6890620b5619de1d15754a396f47bdd9513a4
Ricky Yang <rickyxiaoyang@gmail.com>
2025-05-03 10:33:16 -0400

    Initial Commit

---
+++ xfer/xferApp.swift
@@ -0,0 +1,17 @@
+//
+//  xferApp.swift
+//  xfer
+//
+//  Created by Ricky Yang on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct xferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xferTests/xferTests.swift
@@ -0,0 +1,17 @@
+//
+//  xferTests.swift
+//  xferTests
+//
+//  Created by Ricky Yang on 5/3/25.
+//
+
+import Testing
+@testable import xfer
+
+struct xferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
